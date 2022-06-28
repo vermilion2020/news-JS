5436d254c7f95ab703a6e7950949c19699391e37
Mila <99rest9900@gmail.com>
2022-06-28 13:28:50 +0300

    feat: add data models

---
+++ src/components/model/source.ts
@@ -0,0 +1,14 @@
+export interface ISource {
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
+}
+
+export interface ISourcesData {
+  sources: [ISource];
+  status: string;
+}
